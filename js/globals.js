49c7be74b69d98044dd1d46e14f0af18177bafd0
youngsoo-math <youngsoo.kim.math@gmail.com>
2025-09-21 13:44:08 -0500

    Create globals.js

---
+++ js/globals.js
@@ -0,0 +1,6 @@
+// Declare all global variables here
+let gridCells = [];         // 2D array of <td>
+let selectedCell = null;    // currently highlighted cell
+let selectedRow = null;
+let selectedCol = null;
+let selectedNumber = null;  // number selected via button or keyboard
